8dd3ac13f9d44d3f5653b8aa2ab371a15d1d2b44
FuanjiaRyan <fuanjiaakemfua@gmail.com>
2025-10-19 10:57:46 +0100

    added buyer list

--- lib/views/side_bar_screens/buyers_screen.dart
+++ lib/views/side_bar_screens/buyers_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_web_ryan_store_app/views/side_bar_screens/widgets/buyer_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class BuyersScreen extends StatelessWidget {
@@ -6,8 +7,51 @@ class BuyersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Text('Buyers Screen'),
+    Widget rowHeader(int flex, String text) {
+      return Expanded(
+        flex: flex,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade700),
+            color: Color(0xff3c55ef),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Manage Buyers',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 15),
+            Row(
+              children: [
+                rowHeader(1, 'Image'),
+                rowHeader(3, 'Full Name'),
+                rowHeader(2, 'Address'),
+                rowHeader(2, 'Email'),
+              ],
+            ),
+            BuyerListWidget(),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/side_bar_screens/orders_screen.dart
+++ lib/views/side_bar_screens/orders_screen.dart
@@ -1,13 +1,60 @@
+import 'package:app_web_ryan_store_app/views/side_bar_screens/widgets/order_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class OrdersScreen extends StatelessWidget {
-  static const String id = '\ordersScreen';
+  static const String id = 'ordersScreen';
   const OrdersScreen({super.key});
+
+  Widget rowHeader(int flex, String text) {
+    return Expanded(
+      flex: flex,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xff3c55ef),
+          border: Border.all(color: Colors.grey.shade700),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Orders screen'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Manage Orders',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              rowHeader(1, 'Image'),
+              rowHeader(3, 'Full Name'),
+              rowHeader(2, 'Address'),
+              rowHeader(1, 'Action'),
+              rowHeader(1, 'Reject'),
+            ],
+          ),
+          OrderListWidget(),
+        ],
+      ),
     );
   }
 }
