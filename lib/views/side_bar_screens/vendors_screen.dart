bf1e6ddc801274d27db0e8c7e55ba202c05e63c7
FuanjiaRyan <fuanjiaakemfua@gmail.com>
2025-10-19 12:57:45 +0100

    added vendor list

--- lib/views/side_bar_screens/vendors_screen.dart
+++ lib/views/side_bar_screens/vendors_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_web_ryan_store_app/views/side_bar_screens/widgets/vendor_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class VendorsScreen extends StatelessWidget {
@@ -6,8 +7,51 @@ class VendorsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Vendors Screen'),
+    Widget rowHeader(int flex, String text) {
+      return Expanded(
+        flex: flex,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade700),
+            color: Color(0xff3c55ef),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Manage Vendors',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 15),
+            Row(
+              children: [
+                rowHeader(1, 'Image'),
+                rowHeader(3, 'Full Name'),
+                rowHeader(2, 'Address'),
+                rowHeader(2, 'Email'),
+              ],
+            ),
+            VendorListWidget(),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/views/side_bar_screens/widgets/vendor_list_widget.dart
@@ -0,0 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class VendorListWidget extends StatelessWidget {
+  const VendorListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> buyersStream =
+    FirebaseFirestore.instance.collection('vendors').snapshots();
+    Widget VendorData(Widget widget, int? flex) {
+      return Expanded(
+        flex: flex!,
+        child: Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+          child: Padding(padding: EdgeInsets.all(8), child: widget),
+        ),
+      );
+    }
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: buyersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: (context, index) {
+            final buyer = snapshot.data!.docs[index];
+            return Row(
+              children: [
+                VendorData(
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Image.network(
+                      'https://www.bing.com/images/search?view=detailV2&ccid=hEn3Gg4F&id=8A5971249442AE1DEE06E2F88DC392DAE36D4A1E&thid=OIP.hEn3Gg4Fy1eM9DW2-7h1_QHaGL&mediaurl=https%3a%2f%2fimages.publicpeople.org%2findex-large%2fman-in-jean-shirt-with-beard.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.8449f71a0e05cb578cf435b6fbb875fd%3frik%3dHkpt49qSw4344g%26pid%3dImgRaw%26r%3d0&exph=717&expw=859&q=man&FORM=IRPRST&ck=0397CA44595AF21ED56925771C72821C&selectedIndex=10&itb=0',
+                    ),
+                  ),
+                  1,
+                ),
+                VendorData(Text(buyer['fullName']), 3),
+                VendorData(Text(buyer['locality']), 2),
+                VendorData(Text(buyer['email']), 2),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
